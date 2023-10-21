2ee11bb8a51971f7f6b916a6101e843b27dd10a3
MannyR00 <mannyramirez1330@gmail.com>
2023-10-20 17:54:58 -0700

    switched vector and indirect

--- sum_harness_instructional/sum_indirect.cpp
+++ sum_harness_instructional/sum_indirect.cpp
@@ -12,6 +12,9 @@ void
 setup(int64_t N, uint64_t A[])
 {
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+    for (int j = 0; j < N-1; j++) {
+        A[j] = rand() % N; // Generate random numbers and store in array A
+    }
 }
 
 int64_t
@@ -20,16 +23,12 @@ sum(int64_t N, uint64_t A[])
 
     int64_t sum = 0;
     printf(" inside sum_vector perform_sum, N=%lld \n", N);
-    for (int j = 0; j < N-1; j++) {
-        A[j] = rand() % N; // Generate random numbers and store in array A
-    }
 
     int64_t index = A[0];
     for (int i = 0; i < N; i++) {
         sum += A[index];  // Adds A[index] to the accumulator
         index = A[index];  // This will assign index ~ A[index]
     }
-
 
     return sum;
 }
