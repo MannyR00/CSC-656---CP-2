6b263ef5da5573bbff49b5b8b4ec85ee0c7bd165
MannyR00 <mannyramirez1330@gmail.com>
2023-10-24 16:10:31 -0700

    changed randomizer

--- sum_harness_instructional/sum_indirect.cpp
+++ sum_harness_instructional/sum_indirect.cpp
@@ -7,24 +7,24 @@
 #include <string.h>
 
 
-
 void 
 setup(int64_t N, uint64_t A[])
 {
+    srand48(time(0));
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
     for (int j = 0; j < N; j++) {
-        A[j] = lrand48() % N; // Generate random numbers and store in array A
+        A[j] = lrand48() % N;
     }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-
+    int64_t index = A[0];
     int64_t sum = 0;
     printf(" inside sum_vector perform_sum, N=%lld \n", N);
+    std::cout << A[1] << std::endl;
 
-    int64_t index = A[0];
     for (int i = 0; i < N; i++) {
         sum += A[index];  // Adds A[index] to the accumulator
         index = A[index];  // This will assign index ~ A[index]
